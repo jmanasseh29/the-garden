a9bdddf28adbbc419b1204749ca3afe24ed7e5f7
Josh Hill <joshua_hill@brown.edu>
2022-12-13 16:57:17 -0500

    fixed drop mapping

--- shaders/simulation/drop_fragment.glsl
+++ shaders/simulation/drop_fragment.glsl
@@ -8,13 +8,14 @@ uniform float radius;
 uniform float strength;
 varying vec2 coord;
 
+#include <utils>
 
 void main() {
   /* Get vertex info */
   vec4 info = texture2D(texture, coord);
 
   /* Add the drop to the height */
-  float drop = max(0.0, 1.0 - length(center * 0.5 + 0.5 - coord) / radius);
+  float drop = max(0.0, 1.0 - length(center / waterWidth + 0.5 - coord) / radius);
   drop = 0.5 - cos(drop * PI) * 0.5;
   info.r += drop * strength;
 
